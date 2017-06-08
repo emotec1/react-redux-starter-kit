e2f2d3d4864f3db88b2b33698468ac8df8168ed7
emotec <adrianek3276@gmail.com>
2017-06-08 14:28:58 +0200

    najbardziej podstawowa wersja

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,11 +2,46 @@ import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
-  </div>
-)
+export default class HomeView extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default HomeView
+    this.state = {
+      progress: -1,
+    }
+
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  componentWillMount() {
+    console.log('mounting component');
+    this.setState({
+      progress: this.state.progress + 1
+    })
+  }
+
+  componentWillUpdate(newProps, newState) {
+    console.log('updating component');
+    console.log(newProps);
+    console.log(newState);
+  }
+
+  incrementCounter() {
+    this.setState({
+      progress: this.state.progress + 1
+    })
+
+    console.log('as');
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Welcome!</h4>
+        <span>Progress is {this.state.progress}</span>
+        <img alt='This is a duck, because Redux!' className='duck' src={DuckImage}/>
+        <button onClick={this.incrementCounter}>Increment</button>
+      </div>
+    )
+  }
+}
